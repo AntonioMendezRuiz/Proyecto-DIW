015b855dcbed33922dff6f879cbedf70c4e8d8aa
Ruben Diaz Duran <TheMilubi@gmail.com>
2020-02-06 11:16:14 +0100

    DIV

--- JS/creaProductos.js
+++ JS/creaProductos.js
@@ -1,12 +1,17 @@
 $(document).ready(() => {
+    var productos
     $.ajax({
         url: "../JSON/productos.json",
         method: "GET",
         contentType: "json",
         success: (respuesta) => {
+            productos = respuesta
+            console.log(productos)
             respuesta.forEach((producto, index) => {
-                $('#seccion').html($('#seccion').html() + `<div id=${index}><img src="${producto.url}"><h3>${producto.producto}</h3><p>${producto.descripcion}</p></div>`)
+                $('#productos').html($('#productos').html() + `<div id=${index}><img src="${producto.url}"><br><br><h3>${producto.producto}</h3><span>${producto.descripcion}</span><br><span>${producto.precio}€</span></div>`)
             })
         }
     });
+    $('#productos > div').css({'background-color': 'blue'})
+
 })
