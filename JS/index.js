c1ae1519a70fd3cd654faaeb477eccc772751368
Ruben Diaz Duran <TheMilubi@gmail.com>
2020-02-19 10:50:43 +0100

    Casi termina audio

--- JS/index.js
+++ JS/index.js
@@ -1,14 +1,14 @@
 
 $(function () {
-    $('#play').click((e) => {
-        $('#play').toggle()
-        $('#pause').toggle()
+    $('.fa-play').click((e) => {
+        $(e.currentTarget).toggle()
+        $(e.currentTarget).next().toggle()
         $(e.currentTarget).parent().prev()[0].play()
 
     })
-    $('#pause').click((e) => {
-        $('#play').toggle()
-        $('#pause').toggle()
+    $('.fa-pause').click((e) => {
+        $(e.currentTarget).toggle()
+        $(e.currentTarget).prev().toggle()
         $(e.currentTarget).parent().prev()[0].pause()
     })
     $('video').each((index, e) => {
@@ -17,19 +17,29 @@ $(function () {
         e.ontimeupdate = () => {
             let cT = e.currentTime, d = e.duration
             let p = cT / d * 100
-            $(e).next().children('#progress').val(p)
+            $(e).next().children('.progress').val(p)
             //console.log(p)
         }
     });
-    $('#progress').each((index, e) => {
+    $('audio').each((index, e) => {
+        console.log(e)
+
+        e.ontimeupdate = () => {
+            let cT = e.currentTime, d = e.duration
+            let p = cT / d * 100
+            $(e).next().children('.progress').val(p)
+            //console.log(p)
+        }
+    });
+    $('.progress').each((index, e) => {
         e.onclick = () => {
             let v = $(e).val()
             let d = $(e).parent().prev()[0].duration
             $(e).parent().prev()[0].currentTime = d * v / 100
         }
     })
-    $('#volume').val($('#volume').parent().prev()[0].volume)
-    $('#volume').each((index, e) => {
+    $('.volume').val($('.volume').parent().prev()[0].volume)
+    $('.volume').each((index, e) => {
         e.onclick = () => {
             let v = $(e).val()
             $(e).parent().prev()[0].volume = v
